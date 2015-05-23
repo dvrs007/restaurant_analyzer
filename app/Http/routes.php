10bb8782b268505aa14ee359a8c762b8c18c7aae
johnsonta87 <johnsonta87@gmail.com>
2015-05-23 13:58:14 -0400

    johnson- testing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,10 +15,10 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+//Route::controllers([
+//	'auth' => 'Auth\AuthController',
+//	'password' => 'Auth\PasswordController',
+//]);
 
 
 
