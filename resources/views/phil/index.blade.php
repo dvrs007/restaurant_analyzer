69b19fb09b92b47b24795e6e459ef8ece62ff32d
JeesooKim <jeesoo.joa@gmail.com>
2015-05-26 12:35:34 -0400

    database connection

--- resources/views/phil/index.blade.php
+++ resources/views/phil/index.blade.php
@@ -17,4 +17,19 @@ Restaurant Analyzer
 
 @section('content')
     <p>This is a website built using Laravel, PHP, and Bootstrap. It's very basic right now and doesn't really do anything, but soon I will connect a database to it and it will be totally mind-boggling.</p>
+    
+    @foreach($items as $item)
+
+<h3>
+        
+        
+        
+        {{ $item->item_name }}
+    
+    </h3>
+
+    <br>
+
+@endforeach
+
 @stop
