2f0478806be18bed2b118d8e3b60dbe76bca8f14
JeesooKim <jeesoo.joa@gmail.com>
2015-05-26 12:10:48 -0400

    order class added-model

--- app/order.php
+++ app/order.php
@@ -1,5 +1,6 @@
 <?php
-
+namespace App;
+use Illuminate\Database\Eloquent\Model;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,6 +12,17 @@
  *
  * @author SuzieQ
  */
-class order {
-    //put your code here
+class order extends Model{
+    protected $table= "items" ;
+    //recipe: table name in the database(.env file)
+    //binding to table called recipes
+    //recipe objects bind to this table
+    
+    //protected $primaryKey= "id";//customize the default setting of the primary key 
+    public $timestamps =false; //from method 1 in the cdcontroller
+    protected $fillable = ['item_name','item_price','item_cost']; 
+    //from method2 in the recipecontroller
+            
+    //modify the default setting in the model.php 
+    //vendor/laravel/framework/src/Illuminate/database/Eloquent/model.php
 }
