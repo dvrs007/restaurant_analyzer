ebe260b7465507bef83ba9fce28daa318231f0e9
johnsonta87 <johnsonta87@gmail.com>
2015-05-26 22:16:09 -0400

    johnson- Lavachart Install (views not working but installed)

--- app/Http/Controllers/johnsonController.php
+++ app/Http/Controllers/johnsonController.php
@@ -1,36 +1,49 @@
 <?php namespace App\Http\Controllers;
 
+use App\Order;
+use Khill\Lavacharts\Lavacharts;
+
 class johnsonController extends Controller {
-
-	/*
-	|--------------------------------------------------------------------------
-	| Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller renders your application's "dashboard" for users that
-	| are authenticated. Of course, you are free to change or remove the
-	| controller as you wish. It is just here to get your app started!
-	|
-	*/
-
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
 	public function __construct()
 	{
 		//$this->middleware('auth');
 	}
 
-	/**
-	 * Show the application dashboard to the user.
-	 *
-	 * @return Response
-	 */
 	public function index()
 	{
-		return view('johnson.index');
+
+$lava = new Lavacharts; // See note below for Laravel
+
+$temperatures = Lava::DataTable();
+
+$temperatures->addDateColumn('Date')
+             ->addNumberColumn('Max Temp')
+             ->addNumberColumn('Mean Temp')
+             ->addNumberColumn('Min Temp')
+             ->addRow(array('2014-10-1', 67, 65, 62))
+             ->addRow(array('2014-10-2', 68, 65, 61))
+             ->addRow(array('2014-10-3', 68, 62, 55))
+             ->addRow(array('2014-10-4', 72, 62, 52))
+             ->addRow(array('2014-10-5', 61, 54, 47))
+             ->addRow(array('2014-10-6', 70, 58, 45))
+             ->addRow(array('2014-10-7', 74, 70, 65))
+             ->addRow(array('2014-10-8', 75, 69, 62))
+             ->addRow(array('2014-10-9', 69, 63, 56))
+             ->addRow(array('2014-10-10', 64, 58, 52))
+             ->addRow(array('2014-10-11', 59, 55, 50))
+             ->addRow(array('2014-10-12', 65, 56, 46))
+             ->addRow(array('2014-10-13', 66, 56, 46))
+             ->addRow(array('2014-10-14', 75, 70, 64))
+             ->addRow(array('2014-10-15', 76, 72, 68))
+             ->addRow(array('2014-10-16', 71, 66, 60))
+             ->addRow(array('2014-10-17', 72, 66, 60))
+             ->addRow(array('2014-10-18', 63, 62, 62));
+
+$linechart = Lava::LineChart('Temps')
+                  ->dataTable($temperatures)
+                  ->title('Weather in October');
+
+         return view('johnson.index')->with('order',$linechart);
 	}
 
 }
