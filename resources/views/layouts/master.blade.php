ace6ed9a546f2e6b826324fd9c6300832bc0d1b5
johnsonta87 <johnsonta87@gmail.com>
2015-05-29 15:06:39 -0400

    johnson- design

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script type="text/javascript" src="{{asset('js/order.js')}}"></script>
     </head>
@@ -13,9 +14,10 @@
         <div id="wrap">
             <div class="container">
                 <ul class="nav nav-tabs">
-                    <li role="presentation"><a href="#">About</a></li>
-                    <li role="presentation"><a href="#">Story</a></li>
-                    <li role="presentation"><a href="#">Gallery</a></li>
+                    <li role="presentation"><a href="#">Cafe</a></li>
+                    <li role="presentation"><a href="#">Menu</a></li>
+                    <li role="presentation"><a href="#">Orders</a></li>
+                    <li role="presentation"><a href="#">Statistics</a></li>
                 </ul>
                 <h2>@yield('maintitle')</h2>
                 @section('sidebar')
@@ -33,40 +35,3 @@
         </div>
     </body>
 </html>
-
-<!-- Stored in app/views/layouts/master.blade.php -->
-
-<html>
-    <head>
-        <title>@yield('title')</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script type="text/javascript" src="{{asset('js/order.js')}}"></script>
-    </head>
-    <body>
-        <div id="wrap">
-            <div class="container">
-                <ul class="nav nav-tabs">
-                    <li role="presentation"><a href="#">About</a></li>
-                    <li role="presentation"><a href="#">Story</a></li>
-                    <li role="presentation"><a href="#">Gallery</a></li>
-                </ul>
-                <h2>@yield('maintitle')</h2>
-                @section('sidebar')
-                This is the master sidebar.
-                @show
-
-                @yield('content')
-            </div>
-            <div id="footer">
-                <div class="container">
-                    <div class="col-md-4">Footer Item</div>
-                    
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
-
