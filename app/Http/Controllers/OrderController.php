3bc59aec7d691d43e85952a8558fdfc0cf561808
JeesooKim <jeesoo.joa@gmail.com>
2015-05-29 00:10:35 -0400

    working on FORM-1

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,14 +1,13 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
 //use Illuminate\Routing\Controller;
 
 use App\Order;
 use Illuminate\Http\Request;  //changed it after adding validation
-//use Request; //object capture request
 
+//use Request; //object capture request
 
 /**
  * Description of RecipeControler
@@ -16,54 +15,46 @@ use Illuminate\Http\Request;  //changed it after adding validation
  * @author SuzieQ
  */
 class OrderController extends Controller {
+
     //put your code here
-    
-   
-    public function index()
-    {
-        //$recipes= Recipe::all();
-        //dd($recipes);
-        //return "recipes";
-        
-        //$recipes= Recipe::all();
-        //return view('recipes.index')-> with('recipes', $recipes);                
-        //go to resources/views/recipes...create index.blade.php file
-        return view('orders.index');
+
+
+    public function index() {
+        $items = Order::all();
+        //dd($items);
+        //return "items";
+
+        return view('orders.index')->with("items", $items);
     }
-    
-    public function show($id){
+
+    public function show($id) {
         //get a single detail
-        $recipe=Recipe::find($id); //helper method to find recipe with id
+        $recipe = Recipe::find($id); //helper method to find recipe with id
         //$recipe=Recipe::where('dish_name' , '=', 'sausage');
-        
-        return view('recipes.show')->with('recipe',$recipe);
-    }   
-        
-    public function create(){        
+
+        return view('recipes.show')->with('recipe', $recipe);
+    }
+
+    public function create() {
         return view('recipes.create');
     }
-    
-   
-    public function store(Request $request){       
-        $this->validate($request, 
-                [
-                    'dish_name' => 'required|min:10',
-                    'dish_ingredients' => 'required',
-                    'dish_steps' => 'required' 
-                    //'dish_steps' => 'required|Integer'                    
-                ]                
-                );
-        
+
+    public function store(Request $request) {
+        $this->validate($request, [
+            'dish_name' => 'required|min:10',
+            'dish_ingredients' => 'required',
+            'dish_steps' => 'required'
+                //'dish_steps' => 'required|Integer'                    
+                ]
+        );
+
         //insert a data into database
         //1, get the request...what were sent using post
         //line 8 added
         //$inputs = Request::all();
-        
         //STEP1: Fetch the inputs(fetch all of the input from create)
-        $inputs =$request -> all(); //added after validation added
-        
+        $inputs = $request->all(); //added after validation added
         //STEP2: Create a recipe and saved it into db
-        
         //method1       
 //        $cd = new Cd;
 //        $cd ->titel = $inputs['titel'];
@@ -71,70 +62,63 @@ class OrderController extends Controller {
 //        $cd->jahr = $inputs['jahr'];
 //        
 //        $cd->save();
-        
-        
         //method2
-        Recipe::create($inputs);        
-                
+        Recipe::create($inputs);
+
         //return "recipe inserted";
         //return $inputs;
-        
         //STEP3: Redierect
         return Redirect('recipes');
     }
-    
+
     //update
-    
-    public function edit($id){
-        $recipe = Recipe::find($id);        
+
+    public function edit($id) {
+        $recipe = Recipe::find($id);
         //$recipe = Recipe::where('dish_id' , '=', $id);
-                
+
         return view('recipes.edit')->with('recipe', $recipe);
-    
     }
-    
-    public function update(Request $request){
+
+    public function update(Request $request) {
         //this is for update 
         //get a single detail
-        
-        $this->validate($request, 
-                [
-                    'dish_name' => 'required|min:10',
-                    'dish_ingredients' => 'required',
-                    'dish_steps' => 'required' 
-                    //'dish_steps' => 'required|Integer'                    
-                ]                
-                );        
-        
+
+        $this->validate($request, [
+            'dish_name' => 'required|min:10',
+            'dish_ingredients' => 'required',
+            'dish_steps' => 'required'
+                //'dish_steps' => 'required|Integer'                    
+                ]
+        );
+
         //STEP1: Fetch the inputs
         //$inputs =  $request -> all(); //added after validation added
-        $inputs = array (
+        $inputs = array(
             'dish_name' => $request['dish_name'],
             'dish_ingredients' => $request['dish_ingredients'],
-            'dish_steps'=> $request['dish_steps']           
+            'dish_steps' => $request['dish_steps']
         );
-        
+
         //STEP2: Store         
         //Recipe::find($request['dish_id'])->update($inputs);
         Recipe::where('dish_id', $request['dish_id'])->update($inputs);
         //helper method to find recipe with id
-        
-        
         //return view('recipes.recipe')->with('recipe',$recipe);
         return Redirect('recipes');
-    }  
-    
-    public function deleteLink($id){
+    }
+
+    public function deleteLink($id) {
         Recipe::where('dish_id', $id)->delete();
         return Redirect('recipes');
     }
-    
-    public function deleteButton(Request $request){
+
+    public function deleteButton(Request $request) {
         Recipe::where('dish_id', $request['dish_id'])->delete();
         return Redirect('recipes');
-        
     }
-    
-}//end of class
 
+}
+
+//end of class
 ?>
--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,11 +1,37 @@
 @extends('layouts.master')
 
 @section('content')
+<h2>List of items</h2>
+<br/>
+<form>
+
+    <div class="input_fields_wrap">
+        <button class="add_field_button">Add More Items</button>
+        <div class="order_fields">
+            <select name="item[]">
+                @foreach($items as $item)
+                <option value="">{{ $item->item_name }}  (${{ $item->item_price}})</option>
+                @endforeach
+            </select>
+
+            <select name="quantity[]">
+                @for($i=1; $i<21;$i++)
+                <option value="{{$i}}">{{$i}}</option>
+                @endfor
+            </select>
+        </div>
+    </div>
+    
+    <input type="submit" value="Order Now" />
+    
+</form>
 
 
 
 
 
-ORDER FORM will be updated
+
+
 
 @stop
+
