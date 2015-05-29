665996aa1fec8c412ccef70d01aa01344e047c1a
johnsonta87 <johnsonta87@gmail.com>
2015-05-29 17:04:24 -0400

    johnson- design

--- resources/views/johnson/index.blade.php
+++ resources/views/johnson/index.blade.php
@@ -31,7 +31,3 @@ Sales Chart
     
     
 @stop
-
-@section('content')
-    <p>This is a website built using Laravel, PHP, and Bootstrap. It's very basic right now and doesn't really do anything, but soon I will connect a database to it and it will be totally mind-boggling.</p>
-@stop
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,38 +13,47 @@
     <body>
         <div id="wrap">
             <div class="container">
-                <nav class="navbar navbar-default">
-                    <div class="container-fluid">
-                      <!-- Brand and toggle get grouped for better mobile display -->
-                      <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                          <span class="sr-only">Toggle navigation</span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Order.Eat.Repeat</a>
-                      </div>
+                <header>
+                    <nav class="navbar navbar-default">
+                        <div class="container-fluid">
+                          <!-- Brand and toggle get grouped for better mobile display -->
+                          <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                              <span class="sr-only">Toggle navigation</span>
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">Order.Eat.Repeat</a>
+                          </div>
 
-                      <!-- Collect the nav links, forms, and other content for toggling -->
-                      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav">
-                          <li><a href="#">Menu</a></li>
-                          <li><a href="#">Orders</a></li>
-                          <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Statistics <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                              <li><a href="#">Sales</a></li>
+                          <!-- Collect the nav links, forms, and other content for toggling -->
+                          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                              <li><a href="#">Menu</a></li>
+                              <li><a href="#">Orders</a></li>
+                              <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Statistics <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                  <li><a href="#">Sales</a></li>
+                                </ul>
+                              </li>
                             </ul>
-                          </li>
-                        </ul>
-                      </div><!-- /.navbar-collapse -->
-                    </div><!-- /.container-fluid -->
-                </nav>
+                          </div><!-- /.navbar-collapse -->
+                        </div><!-- /.container-fluid -->
+                    </nav>
+                </header>
 
                 <div id="main">
                     <div class="row">
-                        <img class="main-photo" src="images/cafe_main_pic.jpg" alt="" />
+                        <div class="img-container">
+                            <img src="images/cafe_main_pic.jpg" alt="" />
+                        </div>
+                        <div class="container-fluid">
+                            <div class="col-md-5">
+                                <h2 class="intro-heading">Order.Eat.Repeat.</h2>
+                            </div>
+                        </div>
                     </div>
                     <div class="container-fluid">
                         <h2>@yield('maintitle')</h2>
