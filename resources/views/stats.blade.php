292138dd26931c46ba339ac71bb7b0cefd860558
johnsonta87 <johnsonta87@gmail.com>
2015-05-30 20:34:57 -0400

    johnson- design

---
+++ resources/views/stats.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('title')
+Restaurant Analyzer
+@stop
+
+@section('maintitle')
+Sales Chart
+
+@stop
+
+@section('sidebar')
+    
+
+    <h1>Statistics on the sales of each item</h1>
+    
+@stop
