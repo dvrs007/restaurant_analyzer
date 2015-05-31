a62395c235924f46030bff901cc09f5c93d57201
JeesooKim <jeesoo.joa@gmail.com>
2015-05-30 23:59:50 -0400

    Order FORM-Complete

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,35 +1,60 @@
 @extends('layouts.master')
 
 @section('content')
-<h2>List of items</h2>
 <br/>
-<form>
+<h2>Orders: item/quantity</h2>
+<br/>
+<form action="">
+    <div class="form-group order-select-container">                
+        <select name="item[]" style="color:black;">
+            @foreach($items as $item)
+            <option value="">{{ $item->item_name }}  (${{ $item->item_price}})</option>
+            @endforeach
+        </select>        
 
-    <div class="input_fields_wrap">
-        <button class="add_field_button">Add More Items</button>
-        <div class="order_fields">
-            <select name="item[]">
-                @foreach($items as $item)
-                <option value="">{{ $item->item_name }}  (${{ $item->item_price}})</option>
-                @endforeach
-            </select>
-
-            <select name="quantity[]">
-                @for($i=1; $i<21;$i++)
-                <option value="{{$i}}">{{$i}}</option>
-                @endfor
-            </select>
-        </div>
+        <select name="quantity[]" style="color:black;">
+            @for($i=1; $i<21;$i++)
+            <option value="{{$i}}">{{$i}}</option>
+            @endfor
+        </select>
+        <a href="#" class="btn btn-xs btn-danger btn-remove">Remove</a>
     </div>
-    
+    <a href="" class="btn btn-sm btn-info btn-add-more-order">Add More</a>
+    <br/><br/>
+
     <input type="submit" value="Order Now" />
-    
 </form>
 
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script>
 
 
+var template = '<div class="form-group order-select-container">' +
+        '<select name="item[]" style="color:black;">' +
+        '@foreach($items as $item)' +
+        '<option value="">' + '{{ $item->item_name }}' + ' ' + '(${{ $item->item_price}})</option>' +
+        '@endforeach' +
+        '</select>' +
+        ' <select name="quantity[]" style="color:black;">' +
+        '@for($i=1; $i<21;$i++)' +
+        '<option value="{{$i}}">{{$i}}</option>' +
+        '@endfor' +
+        '</select>' +
+        ' <a href="#" class="btn btn-xs btn-danger btn-remove">Remove</a>' +
+        '</div>'
 
+$('.btn-add-more-order').on('click', function (e) {
+    e.preventDefault();
 
+    $(this).before(template);
+});
+
+$(document).on('click','.btn-remove',function(e){
+    e.preventDefault();
+    $(this).parents('.order-select-container').remove();
+    
+});
+</script>
 
 
 
