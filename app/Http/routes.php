04e77ffc670395cbc047d8c361bd107e3883c3f9
Philip James de Vries <phil@dvrsdesign.com>
2015-06-05 17:34:32 -0400

    phil's url changed

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,7 +24,7 @@ Route::get('home', 'HomeController@index');
  PHIL'S ROUTES
  *************************************************************************/
 
-Route::get('phil', 'philController@index');
+Route::get('server-stats', 'philController@index');
 
 
 /**************************************************************************
