cd5bc3c1e1625037ac83e56451fb0012246c21bb
johnsonta87 <johnsonta87@gmail.com>
2015-06-09 11:20:47 -0400

    johnson-

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,6 +1,6 @@
 <!-- Stored in app/views/layouts/master.blade.php -->
 
-<html ng-app="myApp">
+<html>
     <head>
         <title>@yield('title')</title>
         <meta charset="UTF-8">
@@ -38,7 +38,7 @@
                                     <span class="icon-bar"></span>
                                     <span class="icon-bar"></span>
                                 </button>
-                                <a class="navbar-brand" href="{{ url('/') }}">Order.Eat.Repeat</a>
+                                <a class="navbar-brand" href="{{ url('/') }}">Order.Analyze.Repeat.</a>
                             </div>
 
                           <!-- Collect the nav links, forms, and other content for toggling -->
